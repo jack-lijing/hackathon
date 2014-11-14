316b8f3b93de921e9720dc8b7842353f2d437ae7
jack-lijing <lijing48301243@gmail.com>
2014-11-14 19:53:50 +0800

    ok

--- comtrade.sh
+++ comtrade.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+#cset="76 124"			#贸易国参数集合
+cset="76 124 156 251 392 410 643 699 826 842"
+re=$1
+ti=$2
+HOME=`pwd`
+otop="data"
+ypath=$HOME/$otop/$re/$ti
+
+filed=${re}_${ti}
+filea=${re}_${ti}
+filep=${re}_${ti}_p
+
 #=========去表头\选字段\\排序\有效性核对\填零
 function checkdata()
 {
 	tmp=${1}.e
-	sed '1d' ${1} | sed 's/\".*\"/product/g' | sed 's/\/\/.*,[0-9]+,/,5,/g'| awk -F "," '{ print $15 ","$21}' | column -t -s "," > $tmp
+	sed '1d' ${1} | sed 's/\".*\"/product/g' | sed 's/\/\/.*,[0-9]+,/,5,/g'| awk -F "," '{ printf "%d %s\n",$15,$21}' | column -t -s "," > $tmp
 	sort -n $tmp -o $tmp
 #检查金额字段是否为数值,如不是则退出程序
 	awk '
@@ -14,7 +26,8 @@ function checkdata()
 		}' $tmp	
 
 #1-99,如某类别缺失,则补全,并把value置0
-	join -a 2 $tmp list > $tmp.tmp
+#seq 1 99 >list
+	seq 1 99 | join -a 2 $tmp - > $tmp.tmp
 	awk 'NF==2{print $1" "$2}NF==1{print $1" 0"}' $tmp.tmp >$tmp
 	rm $tmp.tmp
 }
@@ -26,11 +39,11 @@ function checkdata()
 # 函数生成report和parter之间的Import/Export%的表单  $1_$2_2013.t
 function trimdata()
 {
-	import=$1_$2_2013
+	import=$1_$2
 	checkdata	${import}
 	echo "$1 Import $2 Total:$(awk '{t+=$2}  END {print t}' ${import}.e)"
 
-	exp=$2_$1_2013
+	exp=$2_$1
 	checkdata	${exp}
 	echo "$2 Export $1 Total:`awk '{t+=$2}  END {print t}' ${exp}.e`"
 
@@ -40,39 +53,35 @@ function trimdata()
 	$3!=0 { per=$2/$3*100	
 		printf"%s\t%s\t%s\t%d\n",$1,$2,$3,per 
 	} 
-	$3==0 {print $1"\t"$2"\t"$3"\t0"}' | column -t | tee  ${1}.${2}
+	$2==0&&$3==0 {print $1"\t"$2"\t"$3"\t100"}
+	$2!=0&&$3==0 {print $1"\t"$2"\t"$3"\t1000"}' | column -t | tee  ${1}.${2}
 	#	ls | egrep '\.[aiec]' | xargs rm
 }
 
 #__________________________________Programme Start Here
-if [ 1 != $# ]; then
-	echo "Usage $0 country"
+if [ 2 != $# ]; then
+	echo "Usage $0 country time"
 	exit	1
 fi
 
-cset="76 124 156 251 392 410 643 699 826 842"
-re=$1
-od=data
-mkdir -p ${re}				#不存在则创建data目录, 存放原始数据
-cp list ${re}/
-cd ${re}
 
-#seq 1 99 >list
-[ -f ${re} ] && mv ${re} ${re}.old
+#[ -f ${re} ] && mv ${re} ${re}.old
 
 echo "Download import trade date of ${re}"
 for p in $(echo $cset)
 do
-	if [ ${re} == ${p} ] ; then
-			continue
-	fi
-
-	file="${re}_${p}_2013"
-	filex="${p}_${re}_2013"
+#	if [ ${re} == ${p} ] ; then
+#			continue
+#	fi
+	target=${HOME}/${otop}/${re}/${ti}/${p}
+	mkdir -p ${target}
+	cd ${target}
+	file="${re}_${p}"
+	filex="${p}_${re}"
 	#cc=AG2取两位分类代码 cc=AG1取一位代码,以此类推,最大6位
-	urlim=http://comtrade.un.org/api/get\?freq\=A\&ps\=2013\&r\=${re}\&p\=${p}\&rg\=1\&head=m\&fmt\=csv
-	urlex=http://comtrade.un.org/api/get\?freq\=A\&ps\=2013\&r\=${p}\&p\=${re}\&rg\=2\&head=m\&fmt\=csv
-	echo "Deal ${p}"
+	urlim=http://comtrade.un.org/api/get\?freq\=A\&ps\=${ti}\&r\=${re}\&p\=${p}\&rg\=1\&head=m\&fmt\=csv
+	urlex=http://comtrade.un.org/api/get\?freq\=A\&ps\=${ti}\&r\=${p}\&p\=${re}\&rg\=2\&head=m\&fmt\=csv
+	echo "Deal ${p} $ti"
 	if [ -f ${file} ] && [ -f ${filex} ]
 	then
 		echo "Data file exist"
@@ -85,22 +94,26 @@ do
 		curl ${urlex} -o ${filex}
 	fi	
 	trimdata  ${re} ${p} 
+	cd -
 done
 
 #将某国对各国进出口数据整合成总表
-[ -f ${re}.all ] && rm ${re}.all
-touch ${re}.all
+cd ${HOME}/${otop}
+[ -f ${filea} ] && rm ${filea}
+touch ${filea}
 
 for p in $(echo $cset)
 do
-	if [ -f ${re}.${p} ]
+	if [ -f ${ypath}/${p}/${re}.${p} ]
 		then
-		join -a 2 ${re}.all ${re}.${p} >  tmp
-		mv tmp ${re}.all
+		join -a 2 ${filea} ${ypath}/${p}/${re}.${p} >  tmp
+		mv tmp ${filea}
 	fi
 done
 echo "=========================Total Table====================== "
-column -t ${re}.all
+column -t ${filea}
+
+sed -i '26,$d' $filea
 
 #将百分比提取出单独成表,以便R使用 (NR <26 只取农业分类部分)
 awk 'NR<26{
@@ -108,11 +121,11 @@ awk 'NR<26{
 		printf "%d ",$i
 	}
 	printf "\n"
-}' ${re}.all |column -t >${re}.p
+}' ${filea} |column -t >${filep}
 echo "=========================Total Percent====================== "
-cat ${re}.p
-R --slave --vanilla --file=../z.R --args ${re} > R.out
+cat ${filep}
+#R --slave --vanilla --file=../z.R --args ${re} > R.out
+#cd ${HOME}/$otop
+R --vanilla --file=${HOME}/z.R --args ${filea} $filep 
 
-mkdir -p ../out				#存放最终的输出结果
-cp ${re}.* ../out/
 echo "------------------Finished"
--- z.R
+++ z.R
@@ -1,9 +1,36 @@
-args<-commandArgs(T)
-pdf(paste(args[1],".pdf", sep=""))
-t<-read.table(paste(args[1],".p",sep=""),header=TRUE)
-plot(t$X0,t$X842, type="n",xlab="Type",ylab="Per",ylim=c(0,200),main="China Import In 2013")
-for (i in 2:10){
-	lines(t[,1],t[,i],col=i,lty=i-1)
+#读取报告国的总表,计算import/export平均值
+aveline<-function(x){
+	m<-as.matrix(read.table(r, header=T))
+	mlen<-ncol(m)
+	im<-0
+	ex<-0
+	for(i in seq(2,mlen,by=3))
+	{
+		im<-m[,i]+im
+		ex<-m[,i+1]+ex
+	}
+	per<-(100*im)%/%ex
 }
-legend("topleft",dimnames(t)[[2]],col=2:10,lty=1:9,box.col="white")
-dev.off()
+
+#将曲率表生成图像
+getPicture<-function(x)
+{
+	pdf(paste(r,".pdf", sep=""))
+	plot(t$X0,t[[2]], type="n",xlab="Type",ylab="Per",ylim=c(0,200),main=(paste(r, "Import/export ")))
+	len<-length(t)
+	for (i in 2:len){
+		lines(t[,1],t[,i],col=i,lty=i)
+	}
+	lines(t[,1],t[,len],col=1,lwd=5,lty=1)
+	legend("topleft",dimnames(t)[[2]],col=1:len,lty=1:len,box.col="white")
+	dev.off()
+}
+
+args<-commandArgs(T)
+r<-args[1]
+#r<-"china"
+t<-read.table(paste(r,"_p",sep="" ),header=TRUE)
+t$ave<-aveline(r)
+write.table(t,paste(r,"_p",sep=""),row.names=F)
+getPicture(t)
+#browser()
